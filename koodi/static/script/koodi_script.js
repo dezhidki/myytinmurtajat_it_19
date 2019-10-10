39873cf41bed77acbb5b3549c314eeb7c04e34c8
dezhidki <me@deni.sh>
2019-10-10 19:14:10 +0300

    Implement matrix background effect

--- koodi/static/script/koodi_script.js
+++ koodi/static/script/koodi_script.js
@@ -1,13 +1,59 @@
-function startBgEffect() {
-    const canvas = document.getElementById("bg-effect");
-    const ctx = canvas.getContext("2d");
+let matrixCanvas = null;
+let ctx = null;
+let cols = null;
 
-    const w = canvas.width = window.innerWidth;
-    const h = canvas.height = window.innerHeight;
+const fps = 20;
+const waitMs = 1000 / fps;
+let start = null;
+
+const HEIGHT_LIMIT = 10000;
+function matrix(timeStamp) {
+    if (start && timeStamp - start < waitMs) {
+        window.requestAnimationFrame(matrix);
+        return;
+    }
+    start = timeStamp;
+
+    ctx.fillStyle = "#0001";
+    ctx.fillRect(0, 0, matrixCanvas.width, matrixCanvas.height);
+
+    ctx.fillStyle = "#0f0";
+    ctx.font = "15pt monospace";
+
+    for (let i = 0; i < cols.length; i++) {
+        const y = cols[i];
+
+        ctx.fillText(String.fromCharCode(Math.random() * 128), i * 20, y);
+        cols[i] = y > 100 + Math.random() * HEIGHT_LIMIT ? 0 : y + 20;
+    }
+
+    window.requestAnimationFrame(matrix);
+}
+
+function startBgEffect() {
+    matrixCanvas = document.getElementById("bg-effect");
+    ctx = matrixCanvas.getContext("2d");
+
+    matrixCanvas.width = window.innerWidth;
+    matrixCanvas.height = window.innerHeight;
 
     ctx.fillStyle = "#000";
-    ctx.fillRect(0, 0, w, h);
+    ctx.fillRect(0, 0, matrixCanvas.width, matrixCanvas.width);
+    cols = Array(Math.floor(matrixCanvas.width / 20) + 1).fill(0);
+
+    window.requestAnimationFrame(matrix);
 }
+
+window.onresize = e => {
+    if (matrixCanvas) {
+        matrixCanvas.width = window.innerWidth;
+        matrixCanvas.height = window.innerHeight;
+
+        let newColsCount = Math.floor(matrixCanvas.width / 20) + 1;
+        if (newColsCount > cols.length)
+            cols = cols.concat(Array(newColsCount - cols.length).fill(0));
+    }
+};
 
 window.onload = () => {
     startBgEffect();
