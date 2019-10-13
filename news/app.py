ea26a18467573521358f80d2232afd9d3d50ecad
dezhidki <me@deni.sh>
2019-10-13 11:40:37 +0300

    news: randomize years; hide the solution text

--- news/app.py
+++ news/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
+correct_years = ["2013", "1919", "1957", "1998", "1971", "1971", "1936"]
+
 @app.route("/")
 def index():
-    return render_template("main.html")
+    return render_template("main.html", years=random.sample(correct_years, len(correct_years)))
