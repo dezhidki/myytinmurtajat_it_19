83b67e35464f8749cc37ab26324b25b5fb41dcc6
dezhidki <me@deni.sh>
2019-10-13 12:03:42 +0300

    news: fix scoping issue

--- news/app.py
+++ news/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import random
 
 app = Flask(__name__)
 
 correct_years = ["2013", "1919", "1957", "1998", "1971", "1971", "1936"]
 
+@app.route("/check")
+def check_solution():
+    solution_data = request.json()
+    return jsonify(solution_data)
+
+
 @app.route("/")
 def index():
-    return render_template("main.html", years=random.sample(correct_years, len(correct_years)))
+    return render_template("main.html",
+                           years=random.sample(correct_years,
+                                               len(correct_years)))
+
