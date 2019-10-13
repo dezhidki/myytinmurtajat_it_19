32391d34ed424e973a2064942577a364a95c11d2
dezhidki <me@deni.sh>
2019-10-13 13:12:21 +0300

    news: implement solution checking

--- news/app.py
+++ news/app.py
@@ -5,10 +5,23 @@ app = Flask(__name__)
 
 correct_years = ["2013", "1919", "1957", "1998", "1971", "1971", "1936"]
 
-@app.route("/check")
+
+@app.route("/check", methods=["POST"])
 def check_solution():
-    solution_data = request.json()
-    return jsonify(solution_data)
+    solution_data = request.json
+
+    if solution_data["solutions"] != correct_years:
+        return jsonify({
+            "correct": False,
+            "renderContent": None
+        })
+
+    return jsonify({
+        "correct": True,
+        "renderContent": render_template("solution_comment.html",
+                                         solution_fifth_char="A",
+                                         solution_sixth_char="B")
+    })
 
 
 @app.route("/")
@@ -16,4 +29,3 @@ def index():
     return render_template("main.html",
                            years=random.sample(correct_years,
                                                len(correct_years)))
-
--- news/static/script/script.js
+++ news/static/script/script.js
@@ -1,5 +1,51 @@
 const Swap = Swappable.default;
 
+function scrollToElement(element, duration, callback) {
+    let rect = element.getBoundingClientRect();
+    let start = document.documentElement.scrollTop,
+        change = rect.y - rect.height,
+        currentTime = 0,
+        increment = 20;
+
+    let animateScroll = () => {
+        currentTime += increment;
+        document.documentElement.scrollTop = easeInOutQuad(currentTime, start, change, duration);
+        if (currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        } else if (callback)
+            callback();
+    };
+    animateScroll();
+}
+
+function easeInOutQuad(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+};
+
+async function checkSolution(currentSolutions) {
+    let status = await fetch("/check", {
+        method: "POST",
+        body: JSON.stringify({
+            solutions: currentSolutions
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    let solutionResult = await status.json();
+
+    if (solutionResult.correct) {
+        scrollToElement(document.getElementById("comments"), 1000, () => {
+            let currentComments = document.getElementById("current-comments");
+            currentComments.innerHTML = `${solutionResult.renderContent}${currentComments.innerHTML}`;
+        });
+    }
+}
+
 window.onload = () => {
     const swappables = document.querySelectorAll("span.swap-year");
     const swappable = new Swap(swappables, {
@@ -15,5 +61,14 @@ window.onload = () => {
         swappables.forEach(s => {
             s.classList.remove("swappable-place");
         });
+
+        const currentSolutions = document.querySelectorAll("span.swap-year span");
+        let solutions = [];
+        currentSolutions.forEach(v => {
+            if (v.classList.length != 0 && !v.classList.contains("draggable-source--is-dragging"))
+                return;
+            solutions.push(v.textContent);
+        });
+        checkSolution(solutions);
     });
 };
