b0949d485dcae48f7c7b1468358c09a27225737e
dezhidki <me@deni.sh>
2019-10-13 20:07:00 +0300

    koodi: change gpio enable pin

--- koodi/extra_challenge/real.py
+++ koodi/extra_challenge/real.py
@@ -7,16 +7,16 @@ class ExtraChallenge:
 
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(12, GPIO.OUT)
+        GPIO.setup(40, GPIO.OUT)
 
     def start(self):
         if self.started:
             return
         self.started = True
 
-        GPIO.output(12, GPIO.HIGH)
-        time.sleep(200)
-        GPIO.output(12, GPIO.LOW)
+        GPIO.output(40, GPIO.HIGH)
+        time.sleep(0.2)
+        GPIO.output(40, GPIO.LOW)
 
     def stop(self):
         if not self.started:
